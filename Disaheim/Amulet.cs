ff076d996ae3aa094edcdb8726ced23fd8221e5b
Andreas <andreas.loenberg11@gmail.com>
2025-10-08 11:34:46 +0200

    Amulet, Book and Level

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        public string ItemId;
+        public string Design;
+        public Level Quality;
+
+
+
+        public Amulet(string itemId) :
+        this (itemId, Level.Medium, "")
+        {
+        }
+
+        public Amulet(string itemId, Level quality) : //dette er en constructor
+        this (itemId, quality, "")
+        {
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Design = design;
+            Quality = quality;
+        }
+
+        public override string ToString()
+        {
+            return $"itenId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+
+
+    }
+}
+
+
+
+
+/*public Account (string inName, string inAddress, 
+decimal inBalance)  
+{ 
+name = inName; 
+address = inAddress; 
+balance = inBalance; 
+} 
+public Account ( string inName, string inAddress ) : 
+this (inName, inAddress, 0 ) 
+{ 
+} 
+public Account ( string inName ) :  
+this (inName, "Not Supplied", 0 )  
+{ 
+}
+*/
