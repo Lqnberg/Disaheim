7d8e77b71ade6de826b5af05bb5e0f9d600c9138
Andreas <andreas.loenberg11@gmail.com>
2025-10-09 10:12:37 +0200

    ny class/branch

---
+++ Disaheim/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+
+            
+               if (amulet.Quality == Level.Low)
+                {
+                    return 12.5;
+                }
+                else if (amulet.Quality == Level.Medium)
+                {
+                    return 20.0;
+                }
+            else //returnerer high fordi det er den eneste som er tilbage
+            {
+                    return 27.5;
+                }
+            
+        }
+    }
+}
